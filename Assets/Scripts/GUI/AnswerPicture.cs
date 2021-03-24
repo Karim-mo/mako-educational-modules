c412575abd6453ddd9fb10b5c571203f1c836afb
Suicidal <karim.mohamed420@gmail.com>
2021-03-24 16:18:38 +0200

    24/3/2021 - Created Text to Picture Question Template

---
+++ Assets/Scripts/GUI/AnswerPicture.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+public class AnswerPicture : MonoBehaviour
+{
+    public Image buttonImage;
+    [HideInInspector]
+    public bool Correct;
+
+    public void initButton(Sprite img, bool correct)
+    {
+        buttonImage.sprite = img;
+        Correct = correct;
+        gameObject.GetComponent<Button>().onClick.AddListener(() =>
+        {
+            if (Correct)
+            {
+                // Send positive feedback to robot
+                // Update game manager with correct answer to proceed with the questions
+                Debug.Log("This is the correct answer");
+            }
+            else
+            {
+                // Send Negative feedback to robot
+                Debug.Log("This is the wrong answer");
+            }
+        });
+    }
+}
---
+++ Assets/Scripts/PictureQuestionMaker.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PictureQuestionMaker : MonoBehaviour
+{
+    [System.Serializable]
+    public class Answer
+    {
+        public Sprite image;
+        public bool Correct;
+    }
+    [Header("Setup")]
+    public GameObject Question;
+    public GameObject answerImagePrefab;
+    public GameObject rowHolder;
+
+    [Header("Create your Question"), TextArea()]
+    public string QuestionText;
+
+    [Header("Create your Answers")]
+    public Answer[] Answers;
+
+    void Start()
+    {
+        if(Answers.Length > 4)
+        {
+            Debug.LogError("Picture answer array size cannot exceed 4.");
+            return;
+        }
+        initUI();
+    }
+
+    void initUI()
+    {
+        Question.GetComponent<QuestionText>().initQuestionText(QuestionText);
+
+        int len = Answers.Length;
+        for (int i = 0; i < Mathf.Min(4, len); i++)
+        {
+            GameObject tempAnswer = Instantiate(answerImagePrefab, rowHolder.transform);
+            tempAnswer.GetComponent<AnswerPicture>().initButton(Answers[i].image, Answers[i].Correct);
+        }
+    }
+}
